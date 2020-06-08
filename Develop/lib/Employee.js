121f55d9cf1ebe34ec22595197902768bb9d28a5
Tcreamer <trentoncreamer@yahoo.com>
2020-06-07 21:48:07 -0700

    HW Updates

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,14 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+  }
+  getName() {
+    // from inside the function and passing it back out
+    return this.name;
+  }
+}
+
+module.exports = Employee;
